b623d5d37906ba7f7ae945cea37c4e2e99c53d55
Cyril Grandjean <info@cyril-grandjean.fr>
2016-03-05 13:50:09 +0100

    Update keys for Travis

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
-    gpg --fast-import cd/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_3222f085f910_key -iv $encrypted_3222f085f910_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc.asc -d
+    gpg --fast-import cd/codesigning.asc
 fi
