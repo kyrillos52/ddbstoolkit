8e4d269a959dcd5b2cd9f840c8c330838236a7de
Cyril Grandjean <info@cyril-grandjean.fr>
2016-06-29 22:04:18 +0200

    Update keys GPG

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -2,5 +2,5 @@
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	openssl aes-256-cbc -K $encrypted_a53a042b3b62_key -iv $encrypted_a53a042b3b62_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
 	gpg --fast-import cd/codesigning.asc
-	gpg --keyserver hkp://keyserver.ubuntu.com --send-keys $encrypted_a53a042b3b62_key
+	gpg --keyserver hkp://keyserver.ubuntu.com --send-keys 534f7c1a7eb0a0e4
 fi
