e8ce9d135226993d56d48309577560d396a632a7
Cyril Grandjean <info@cyril-grandjean.fr>
2016-07-16 11:39:16 +0200

    Code coverage

---
+++ cd/code-coverage.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+	curl https://www.jpm4j.org/install/script | sh
+	jpm install com.codacy:codacy-coverage-reporter:assembly
+	export CODACY_PROJECT_TOKEN=${env.CODACY_PROJECT_TOKEN}
+	codacy-coverage-reporter -l Java -r coverage.xml
+fi
