e993315eaac23b43f427df80f04b65c699d0d001
Cyril Grandjean <info@cyril-grandjean.fr>
2017-09-20 21:17:40 +0200

    Remove cyclic dependencies

--- ddbstoolkit-jdbc/src/test/java/org/ddbstoolkit/toolkit/jdbc/JDBCConcreteDatabaseTest.java
+++ ddbstoolkit-jdbc/src/test/java/org/ddbstoolkit/toolkit/jdbc/JDBCConcreteDatabaseTest.java
@@ -5,12 +5,77 @@ import org.ddbstoolkit.toolkit.core.exception.DDBSToolkitException;
 import org.ddbstoolkit.toolkit.core.IEntity;
 import org.ddbstoolkit.toolkit.core.exception.DDBSToolkitException;
 import org.ddbstoolkit.toolkit.jdbc.JDBCModuleTest;
-import org.ddbstoolkit.toolkit.modules.datastore.mysql.DistributedMySQLTableManager;
-import org.ddbstoolkit.toolkit.modules.datastore.mysql.MySQLConnector;
 import org.junit.After;
 import org.junit.Before;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCConcreteDatabaseTest extends JDBCModuleTest {
+
+    class MySQLConnector extends JDBCConnector {
+
+        /**
+         * MySQL JDBC Driver name
+         */
+        private static final String MYSQL_DRIVER = "org.gjt.mm.mysql.Driver";
+
+        /**
+         * MySQL Database URL
+         */
+        private String url;
+
+        /**
+         * MySQL User Login
+         */
+        private String login;
+
+        /**
+         * MySQL User Password
+         */
+        private String password;
+
+        /**
+         * JDBC Constructor
+         * @param connector Connector
+         */
+        public MySQLConnector(Connection connector) {
+            super(connector);
+        }
+
+        /**
+         * Instantiate a MySQL connector
+         * @param url MySQL Database URL
+         * @param login MySQL User Login
+         * @param password MySQL User Password
+         * @throws ClassNotFoundException Class not found exception
+         */
+        public MySQLConnector(final String url, final String login, final String password) throws ClassNotFoundException {
+            super(url);
+            this.url = url;
+            this.login = login;
+            this.password = password;
+
+            Class.forName(MYSQL_DRIVER);
+        }
+
+        /**
+         * Open connection to the database
+         * @throws SQLException SQL Exception
+         */
+        @Override
+        public void open() throws SQLException {
+            connector = DriverManager.getConnection(url,login,password);
+        }
+    }
+
+    class DistributedMySQLTableManager extends JDBCEntityManager {
+        public DistributedMySQLTableManager(MySQLConnector myConnector) {
+            super(myConnector);
+        }
+    }
+
 
     /**
      * JDBC String
